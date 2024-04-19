c6750bff87466188f313bdea1bec8686601f1252
MiniNija <anishkute12@gmail.com>
2024-04-19 21:10:06 +0530

    Chapter 2 - Assignment 1 - Crouch

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-   private void Update()
+    private void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
@@ -20,17 +20,44 @@ public class PlayerController : MonoBehaviour
 
         Vector3 scale = transform.localScale;
         if (speed < 0)
-        {      
-           scale.x = -1f * Mathf.Abs(scale.x);
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
         }
         else if (speed > 0)
-            {
-                scale.x = Mathf.Abs(scale.x);
-            }
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
         transform.localScale = scale;
-        
 
-        //bool jump = Input.GetAxisRaw("Space");
-        //animator.SetBool("Jump" , jump);
+
+
+        float vertical = Input.GetAxisRaw("Vertical");
+        if (vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", true);
+            // Resize the collider here
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+            // Reset the collider size here
+        }
+
+
+
     }
+
+
+
+
+
 }
