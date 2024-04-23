5dc04eb2b6553e2faa65eabdd1bf4b2a0f79bf00
MiniNija <anishkute12@gmail.com>
2024-04-23 08:29:58 +0530

    To the Next level
    When the player collides with the empty game object loading the next level.

---
+++ Assets/Script/LevelOverController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelOverController : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Level finished");
+            LoadNextLevel();
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
